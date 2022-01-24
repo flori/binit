d032135b89db109a5fac21c0d579bcdc779b9683
Florian Frank <flori@ping.de>
2022-01-24 16:56:04 +0100

    Add program name to log output

--- log.go
+++ log.go
@@ -6,11 +6,13 @@ import (
 	"time"
 )
 
+const PROGRAM_NAME = "binit"
+
 type logWriter struct{}
 
 func (writer logWriter) Write(bytes []byte) (int, error) {
 	// Switch logging to stdout as well
-	return fmt.Print(time.Now().Format(time.RFC3339) + " " + string(bytes))
+	return fmt.Printf("%s %s: %s", time.Now().Format(time.RFC3339), PROGRAM_NAME, string(bytes))
 }
 
 func ConfigureLogging() {
